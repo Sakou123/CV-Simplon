ef5b527a2f22cf0783cd4731978caa39dd8427d6
Sakou <vigneronlucweb@gmail.com>
2021-06-30 16:49:30 +0200

    Commentaire

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,8 +2,9 @@ function Button() {
   var button = document.getElementById("button")
   var element = document.body;
   element.classList.toggle("dark-mode");
-  if (element.classList.contains("dark-mode")){
-    let msg = "On rallume plutôt ?";
+  
+  if (element.classList.contains("dark-mode")){         /* Check si le body est en darkmode pour */
+    let msg = "On rallume plutôt ?";                    /* <-- changer le texte du bouton */
     button.innerHTML = msg;
   }
   else{
