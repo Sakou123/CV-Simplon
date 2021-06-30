7d867ed70b886c9cf7e7b9d451615d59406c61e3
Sakou <vigneronlucweb@gmail.com>
2021-06-30 14:23:04 +0200

    End

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,13 @@
 function Button() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+  var button = document.getElementById("button")
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  if (element.classList.contains("dark-mode")){
+    let msg = "On rallume plutôt ?";
+    button.innerHTML = msg;
   }
+  else{
+    let msg = "On éteint la lumière ?";
+    button.innerHTML = msg;
+  }
+}
